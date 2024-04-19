2f7a443b6071458b1269455f94a7ea8b66966747
Peter <klintsov.p@mail.ru>
2024-04-19 13:48:25 +0300

    работа по меню создания архивов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,46 @@
-import java.util.Scanner
-
 fun main() {
     val app = App()
     app.start()
 }
 
-class App() {
-    val zips = ArrayList<Zip>()
+class App {
+    private val zips = mutableMapOf<Int, Zip>()
 
 
     fun start() {
         println("Добро пожаловать в приложение \"Заметки\"")
-        menu()
-        val input = Scanner(System.`in`).nextLine()
-        while (true) {
-            
+        zipMenu()
+        when (val choice = checkingInput(zips.size)) {
+            0 -> makeZip()
+            zips.size -> return
+            else -> openZip(choice)
         }
+
     }
 
-    private fun menu() {
+    private fun zipMenu() {
         println("Список архивов:")
         println("0. Создать архив")
         if (zips.isEmpty()) {
             println("1. Выход ")
         } else {
             for (i in zips) {
-                println("${i.number}. ${i.name}")
+                println("${i.key}. ${i.value}")
             }
             println("${zips.size}. Выход ")
         }
+    }
+
+    private fun makeZip() {
+        println("Введите название архива")
+        val zipName = checkingText("Название архива")
+        val zip: ArrayList<String> = arrayListOf("")
+        val newZip = Zip(zips.size - 1, zipName, zip)
+        zips[zips.size] = newZip
+    }
+
+    private fun openZip(choice: Int) {
+        println("Архив \"${zips[choice]}:")
     }
 }
 
@@ -56,14 +68,14 @@ fun checkingInput(size: Int): Int {
 
 fun showMenu() {}
 
-fun checkingText(category: String) {
-    var input: String = readln()
+fun checkingText(category: String): String {
+    var input = readln()
     while (true) {
         if (input == "") {
             println("$category не может быть пустым")
             input = readln()
         } else {
-            break
+            return input
         }
     }
 }
